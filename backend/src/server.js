bc0f155892538d8e641c018a3a4dbf11cea84782
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-08-07 18:47:57 -0300

    Criação da estrutura base e arquivo de rotas

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = require('./routes');
+
+const server = express();
+
+server.use(routes);
+
+server.listen(3333, () => {
+    console.log('backend executando...');
+})
