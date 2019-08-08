d5db890028a25f47b1bc82936c6a2d96155638cc
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-08-08 14:15:18 -0300

    Criação da conexao com o bd

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,4 +6,9 @@ routes.get('/', (req, res) => {
     return res.json({ message: `Olá ${req.query.name}`});
 });
 
+routes.post('/devs', (req, res) => {
+
+    return res.json({ name: req.body.name });
+})
+
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
+
 
 const server = express();
 
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ft28b.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true // Remove o DeprecationWarning
+});
+
+server.use(express.json()); // Faz com que o express interprete requisições do tipo json com valores passados pelo body
 server.use(routes);
 
 server.listen(3333, () => {
